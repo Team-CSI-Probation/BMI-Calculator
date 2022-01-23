baf7b495635e73926d69d77c9b77d2870681a04c
vbansal18 <vaibhavbansal369@gmail.com>
2022-01-23 18:23:42 +0530

    finalising

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.*
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
 
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +17,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var SeekBar= findViewById<SeekBar>(R.id.AgeBar)
+        var ageBox= findViewById<EditText>(R.id.editAge)
 
 
+        SeekBar.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            var progressChangedValue = 0
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                progressChangedValue = progress
+                ageBox.setText(progressChangedValue.toString())
 
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+            }
+        })
 
 
         calculate.setOnClickListener {
 
+
             var height = findViewById<EditText>(R.id.editHeight)
             var weight = findViewById<EditText>(R.id.editWeight)
-
             // Check if the height EditText and Weight EditText are not empty
             if (height.text.isNotEmpty() && weight.text.isNotEmpty()) {
                 val height = (height.text.toString()).toInt()
